b4e7c2c72955f5c9c28127894455b851964e86e1
Todd Jacobus <tj@tcarta.com>
2019-11-10 05:28:01 -0700

    added wait for db command

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,28 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """Test waiting for db until db is available"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+    # Using 'patch' as a decorator is basically the same as using within
+    # the context manager above, however, this allows you to pass in an
+    # argument to your test function.
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """Test waiting for db"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            # simulate not connecting to the db 5 times, then connecting
+            # on the last try.
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)
